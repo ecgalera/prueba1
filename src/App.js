040e2c4d74e16578f5581a7e00e179e3642af801
Eduardo Galera <galera.eduardo@gmail.com>
2023-01-02 18:10:51 -0300

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Primero from "./components/Primero";
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hola Bootstrap</h1>
+     <Primero  saludo={"Hola como estas"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Primero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Primero = (props) => {
+  return (
+    <div>Primero
+    <h2>{props.saludo}</h2>
+    </div>
+  )
+}
+
+export default Primero
